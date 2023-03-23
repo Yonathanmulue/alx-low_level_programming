48614830c76399086846f3a79452c493e0caeddd
yonathan <yonathanmulue738@gmail.com>
2023-03-23 23:27:05 +0000

    Leap year

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -6,9 +6,11 @@
 * @c: third integer
 * Return: largest number
 */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
+
 	if (a > b && a > c)
 	{
 		largest = a;
@@ -25,5 +27,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
