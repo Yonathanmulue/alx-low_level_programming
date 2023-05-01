bff827fa2f5d66df9aa20075833933259815426b
Your Name <you@example.com>
2023-05-01 16:56:09 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,27 +10,36 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *current;
-	unsigned int i;
+	unsigned int j;
+	listint_t *few;
+	listint_t *hood = *head;
 
-	new = malloc(sizeof(listint_t));
-	if (!new)
+	few = malloc(sizeof(listint_t));
+	if (!few || !head)
 		return (NULL);
-	new->n = n;
 
-	current = *head;
-	for (i = 0; current && i < idx - 1; i++)
-		current = current->next;
+	few->n = n;
+	few->next = NULL;
 
-	if (i != idx - 1)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->next = current ? current->next : NULL;
 	if (idx == 0)
-		*head = new;
-	else if (current)
-		current->next = new;
-	return (new);
+	{
+		few->next = *head;
+		*head = few;
+		return (few);
+	}
+
+	for (j = 0; hood && j < idx; j++)
+	{
+		if (j == idx - 1)
+		{
+			few->next = hood->next;
+			hood->next = few;
+			return (few);
+		}
+		else
+			hood = hood->next;
+	}
+
+	return (NULL);
 }
+
