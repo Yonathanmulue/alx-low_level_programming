deff0be40c5a74a3508b7d1e70e0963c5efe08e1
Your Name <you@example.com>
2023-05-02 03:20:32 -0700

    Print(safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,56 +5,56 @@ size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 
 /**
- * looped_listint_len - This function Counts the number of unique nodes
+ * looped_listint_len - Counts the number of unique nodes
  * in a looped listint_t linked list.
- * @head:This is a pointer to the head of the listint_t to check.
+ * @head: A pointer to the head of the listint_t to check.
  *
  * Return: If the list is not looped - 0.
  * Otherwise - the number of unique nodes in the list.
  */
 size_t looped_listint_len(const listint_t *head)
 {
-	const listint_t *person, *fade;
+	const listint_t *tortoise, *hare;
 	size_t codes = 1;
 
 	if (head == NULL || head->next == NULL)
 		return (0);
 
-	person = head->next;
-	fade = (head->next)->next;
+	tortoise = head->next;
+	hare = (head->next)->next;
 
-	while (fade)
+	while (hare)
 	{
-		if (person == fade)
+		if (tortoise == hare)
 		{
-			person = fade;
-			while (person != fade)
+			tortoise = head;
+			while (tortoise != hare)
 			{
 				codes++;
-				person = person->next;
-				fade = fade->next;
+				tortoise = tortoise->next;
+				hare = hare->next;
 			}
 
-			person = person->next;
-			while (person != fade)
+			tortoise = tortoise->next;
+			while (tortoise != hare)
 			{
 				codes++;
-				person = person->next;
+				tortoise = tortoise->next;
 			}
 
 			return (codes);
 		}
 
-		person = person->next;
-		fade = (fade->next)->next;
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
 	}
 
 	return (0);
 }
 
 /**
- * print_listint_safe - We use this function to Prints a listint_t list safely.
- * @head: This is a pointer to the head of the listint_t list.
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
  *
  * Return: The number of nodes in the list.
  */
