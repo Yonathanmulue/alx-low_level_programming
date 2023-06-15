5bbf5ca0e9c5e88d11e6e254e427295a9e131b37
yonatan <yonatanmulue738@gmail.com>
2023-06-16 02:20:54 +0300

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,14 +9,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
+    int node_count = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
-
-    return count;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
 }
